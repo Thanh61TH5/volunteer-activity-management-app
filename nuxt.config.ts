e4a46f4fdd6d823ac1b30e7d33afa02462412e26
Thanh61TH5 <giangthanh25092001@gmail.com>
2024-01-27 01:13:00 +0700

    fixing common 6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,23 +28,37 @@ export default ({
   components: true,
   app: {
     head: {
-      title: '',
+      title: 'Hỗ trợ người già neo đơn',
       meta: [
         {
           name: 'viewport',
           content: 'width=device-width, initial-scale=1.0, height=device-height, minimum-scale=1.0, user-scalable=0',
         },
         {
-          hid: 'description',
-          name: 'description',
-          content: '',
+          hid: 'hệ thống hỗ trợ người già neo đơn',
+          name: 'hệ thống hỗ trợ người già neo đơn',
+          content: 'Hệ thống quản lý hoạt động thiện nguyện h trợ người già neo đơn',
         },
         {
-          hid: 'keywords',
-          name: 'keywords',
-          content: '',
+          hid: 'người già neo đơn',
+          name: 'người già neo đơn',
+          content: 'người già neo đơn',
         },
-
+        {
+          hid: 'hệ thống thiện nguyện',
+          name: 'hệ thống thiện nguyện',
+          content: 'hệ thống thiện nguyện',
+        },
+        {
+          hid: 'thiện nguyện',
+          name: 'thiện nguyện',
+          content: 'thiện nguyện',
+        },
+        {
+          hid: 'tình nguyện',
+          name: 'tình nguyện',
+          content: 'tình nguyện',
+        },
       ],
     },
   },
